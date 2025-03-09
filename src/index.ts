ca7c25c9f4a386695ec4bd62b9b1132cb1acc56a
Mohamed Saad <saaddevlife@gmail.com>
2025-03-09 13:30:36 +0300

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Route imports
+
+// middlewares
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+// Routes
+
+
+// Server
+const portNum = process.env.PORT || 3003;
+app.listen(portNum, ()=>{
+    console.log(`server running on port ${portNum}`)
+})
+
